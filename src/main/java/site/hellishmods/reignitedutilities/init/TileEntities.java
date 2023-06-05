7dc2888a66822521365b3147afbb0306bab442b6
Gcat101 <gcatmail2@gmail.com>
2023-06-05 21:53:47 +0300

    Solar/lunar panels + angel block recipe

--- src/main/java/site/hellishmods/reignitedutilities/init/TileEntities.java
+++ src/main/java/site/hellishmods/reignitedutilities/init/TileEntities.java
@@ -6,14 +6,19 @@ import site.hellishmods.reignitedutilities.lib.tileentities.*;
 import site.hellishmods.reignitedutilities.reignitedutilities;
 
 public class TileEntities {
+    public static RegistryObject<TileEntityType<SolarPanelMillTile>> solar_panel_mill_entity_type;
+    public static RegistryObject<TileEntityType<LunarPanelMillTile>> lunar_panel_mill_entity_type;
     public static RegistryObject<TileEntityType<WindMillTile>> wind_mill_entity_type;
     public static RegistryObject<TileEntityType<FireMillTile>> fire_mill_entity_type;
     public static RegistryObject<TileEntityType<CreativeMillTile>> creative_mill_entity_type;
     public static RegistryObject<TileEntityType<ManualMillTile>> manual_mill_entity_type;
     public static RegistryObject<TileEntityType<DragonEggMillTile>> dragon_egg_mill_entity_type;
+
     public static RegistryObject<TileEntityType<ChunkLoadingWardTile>> chunk_loading_ward_entity_type;
 
     public static void init() {
+        solar_panel_mill_entity_type = reignitedutilities.TILE_ENTITIES.register("solar_panel_mill", () -> TileEntityType.Builder.of(SolarPanelMillTile::new, Blocks.SOLAR_PANEL.get()).build(null));
+        lunar_panel_mill_entity_type = reignitedutilities.TILE_ENTITIES.register("lunar_panel_mill", () -> TileEntityType.Builder.of(LunarPanelMillTile::new, Blocks.LUNAR_PANEL.get()).build(null));
         wind_mill_entity_type = reignitedutilities.TILE_ENTITIES.register("wind_mill", () -> TileEntityType.Builder.of(WindMillTile::new, Blocks.WIND_MILL.get()).build(null));
         fire_mill_entity_type = reignitedutilities.TILE_ENTITIES.register("fire_mill", () -> TileEntityType.Builder.of(FireMillTile::new, Blocks.FIRE_MILL.get()).build(null));
         creative_mill_entity_type = reignitedutilities.TILE_ENTITIES.register("creative_mill", () -> TileEntityType.Builder.of(CreativeMillTile::new, Blocks.CREATIVE_MILL.get()).build(null));
